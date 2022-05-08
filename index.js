38c0ab8ab4da809310263516830afe21c882e894
RidoyMia <Ridoymia153159hero@gmail.com>
2022-05-08 12:52:09 +0600

    update finish

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.PRIVATE_KEY}:${process.env.PRIVATE_PASS}@cluster0.ha4fu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// collection 
+
 async function run(){
   try{
      await client.connect()
